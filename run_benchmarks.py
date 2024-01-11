42dab99bad8258ddff29dc064baadec3f2644546
Felix Grzelka <felix.grzelka@hpi.de>
2024-01-11 17:39:07 +0000

    add mega benchmark

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -10,13 +10,24 @@ def run_notebook_locally(notebook_filename):
         notebook = nbformat.read(file_ptr, as_version=4)
 
     preprocessor = ExecutePreprocessor(timeout=600, kernel_name="python3")
-    preprocessor.preprocess(notebook, {"metadata": {"path": "."}})
+    output = preprocessor.preprocess(notebook, {"metadata": {"path": "."}})
+    print(output)
+
+def run_notebook_with_mega(notebook_filename):
+    os.environ["MEGACLITE_DISABLE"] = "0"
+    with open(notebook_filename) as file_ptr:
+        notebook = nbformat.read(file_ptr, as_version=4)
+
+    preprocessor = ExecutePreprocessor(timeout=600, kernel_name="python3")
+    output = preprocessor.preprocess(notebook, {"metadata": {"path": "."}})
+    print(output)
 
 
 @click.argument("notebook_filename")
 @click.command()
 def main(notebook_filename):
-    run_notebook_locally(notebook_filename)
+    # run_notebook_locally(notebook_filename)
+    run_notebook_with_mega(notebook_filename)
 
 
 if __name__ == "__main__":
