3291f02a312d17251033803f75603fa25559e60a
Felix Grzelka <felix.grzelka@hpi.de>
2024-06-14 12:37:41 +0000

    allways run all benchmarks

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import subprocess
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
-import click
 
 RESULTS_DIR = Path("benchmark-results")
 
@@ -16,8 +15,13 @@ def run_notebook_locally(notebook_filename):
     preprocessor = ExecutePreprocessor(timeout=600, kernel_name="python3")
     preprocessor.preprocess(notebook, {"metadata": {"path": "."}})
 
+    RESULTS_DIR.mkdir(exist_ok=True, parents=True)
     with open(RESULTS_DIR / Path(notebook_filename).name, 'w', encoding='utf-8') as file_ptr:
         nbformat.write(notebook, file_ptr)
+    
+    print("done")
+    import time
+    time.sleep(30)
 
 def run_notebook_with_mega(notebook_filename):
     print("running with mega", notebook_filename)
@@ -31,19 +35,14 @@ def run_notebook_with_mega(notebook_filename):
     preprocessor = ExecutePreprocessor(timeout=600, kernel_name="python3")
     preprocessor.preprocess(notebook, {"metadata": {"path": "."}})
     
+    RESULTS_DIR.mkdir(exist_ok=True, parents=True)
     with open(RESULTS_DIR / Path(notebook_filename).name, 'w', encoding='utf-8') as file_ptr:
         nbformat.write(notebook, file_ptr)
     
     subprocess.run("docker compose down".split(" "))
 
-
-
-
-
-@click.argument("notebook_filename")
-@click.command()
-def main(notebook_filename):
-    repetitions = 3
+def main():
+    repetitions = 1
     benchmark_dir = Path("benchmarks")
     for rep in range(repetitions):
         for benchmark in benchmark_dir.glob("*.ipynb"):
