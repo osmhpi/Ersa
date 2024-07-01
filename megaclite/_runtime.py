a7574feccfb4d5f49388fdcf2a02cb18a2b3339b
Felix Grzelka <felix.grzelka@hpi.de>
2024-07-01 14:07:53 +0000

    run directly inside the module

--- megaclite/_runtime.py
+++ megaclite/_runtime.py
@@ -1,37 +1,24 @@
 """This module loads and runs the gpu training jobs.
 It is supposed to be invoked as a shell script in it's own subprocess."""
 from pathlib import Path
-
-import click
 import dill  # pylint: disable=import-outside-toplevel
-import torch  # pylint: disable=import-outside-toplevel
+import sys
 
+"""Run the training job.
+1. Load the state of the jupyter notebook from the provided file.
+2. Run the actual training script (cell).
+3. Save the model weights to the output path.
+"""
+_, state, cell, output = sys.argv
+# load state from the users notebook
+dill.load_module(state)
+cell_lines = Path(cell).read_text(encoding="UTF-8")
 
-@click.command()
-@click.argument("state")
-@click.argument("cell")
-@click.argument("output")
-def main(state, cell, output):
-    """Run the training job.
-    1. Load the state of the jupyter notebook from the provided file.
-    2. Run the actual training script (cell).
-    3. Save the model weights to the output path.
-    """
-    # load state from the users notebook
-    dill.load_module(state)
-    cell_lines = Path(cell).read_text(encoding="UTF-8")
-
-    apply_pending_tensor_moves()
-    apply_pending_module_moves()
-    # train the model
-    exec(cell_lines)  # pylint: disable=exec-used
-    reverse_module_moves()
-    reverse_tensor_moves()
-    # save the model
-    # torch.save(globals()[model_name].state_dict(), output)
-    dill.dump_module(output)
-
-
-if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    main()
+apply_pending_tensor_moves()
+apply_pending_module_moves()
+# train the model
+exec(cell_lines)  # pylint: disable=exec-used
+reverse_module_moves()
+reverse_tensor_moves()
+# save the state
+dill.dump_module(output)
